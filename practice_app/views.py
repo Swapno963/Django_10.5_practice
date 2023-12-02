99d0951d293c61cb600afdb95f95a505185efc16
swapno963 <swapno963@gmail.com>
2023-12-03 00:11:59 +0600

    some filter added

--- practice_app/views.py
+++ practice_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    d = {'author':'swapno mondol','lst':[1,2,4,2,4],
+         'txt':['This','is','diffrent','text'],
+         'may_empty':'',
+         'firs_cap':'my first letter is not uppercase.'
+         
+         }
+    return render(request,'index.html',d)
